b6ab3e9640faa618e54da5055c63b18a9fe36d1b
Anna Denisova <abachledacurus@gmail.com>
2022-11-28 12:29:09 +0700

    слайдер поправлен

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 let hover = 0;
-const startX = {};
 const items = document.querySelectorAll('.slider__item');
-let def = document.querySelector('.slider__items').offsetWidth * 0.5 - items[0].style.width * 0.5;
-console.log(document.querySelector('.slider__items').offsetWidth)
-//заполнить объект стартовыми координатами для каждого слайда, никак не работает, т.к. неактивные слайды имеют ширину =0
-for (let i = 0; i < items.length; i ++) {
-  startX[i] = def;
-  def = def + items[i].style.width;
-  // console.log(def)
-  console.log(items[i].style.width)
-}
-console.log(startX);
 
 document.querySelector('.slider')
    .addEventListener('mousemove', () => hover = 1);
@@ -37,15 +26,23 @@ setInterval(function() {
             }
             if ((items[i - 1]) && ((i - 1) !== (index - 1))) {
                 items[i - 1].classList.contains('prev') && items[i - 1].classList.remove('prev');
+            } else if (!items[i - 1]) {
+              items[items.length - 1].classList.contains('prev') && items[items.length - 1].classList.remove('prev');
             }
             if ((items[i + 1]) && ((i + 1) !== (index + 1))) {
                 items[i + 1].classList.contains('next') && items[i + 1].classList.remove('next');
+            } else if (!items[i + 1]) {
+              items[0].classList.contains('next') && items[0].classList.remove('next');
             }
           }
     if(items[index - 1]) {
       items[index - 1].style.left = 'unset';
       items[index - 1].style.right = (0.5 * document.querySelector('.slider__items').offsetWidth + 0.5 * items[index].offsetWidth) + 'px';
       items[index - 1].classList.add('prev');
+    } else {
+      items[items.length - 1].style.left = 'unset';
+      items[items.length - 1].style.right = (0.5 * document.querySelector('.slider__items').offsetWidth + 0.5 * items[index].offsetWidth) + 'px';
+      items[items.length - 1].classList.add('prev');
     }
   items[index].classList.add('active');
   if (items[index].style.right) {items[index].style.right = 'unset';}
@@ -54,12 +51,16 @@ setInterval(function() {
     if (items[index + 1].style.right) {items[index + 1].style.right = 'unset';}
     items[index + 1].style.left = (0.5 * document.querySelector('.slider__items').offsetWidth + 0.5 * items[index].offsetWidth) + 'px';
     items[index + 1].classList.add('next');
+  } else {
+    if (items[0].style.right) {items[0].style.right = 'unset';}
+    items[0].style.left = (0.5 * document.querySelector('.slider__items').offsetWidth + 0.5 * items[index].offsetWidth) + 'px';
+    items[0].classList.add('next');
   }
   index++;
   if (index === items.length) {
     index = 0;
   }
-}}, 2000);
+}}, 4000);
 
 
 //плеер
